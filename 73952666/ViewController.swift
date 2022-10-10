7055d65884ec2d37da39cfbe138ddb9810b69322
kasimok <kasimok@163.com>
2022-10-10 20:37:05 +0800

    Initial Commit

---
+++ 73952666/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  73952666
+//
+//  Created by kasimok on 2022-10-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
